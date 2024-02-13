fc38bf71616b00f3c10d8f5a495661af71d298cc
Siddhant Singh Karki <siddhantskarki@gmail.com>
2024-02-13 11:58:03 +1100

    context

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
+import { FeedbackProvider } from './context/FeedbackContext'
+import FeedbackData from './data/FeedbackData'
+
+function App() {
+  const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+
+  return (
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Route exact path='/'>
+            <FeedbackForm handleAdd={addFeedback} />
+            <FeedbackStats feedback={feedback} />
+            <FeedbackList  handleDelete={deleteFeedback} />
+          </Route>
+
+          <Route path='/about' component={AboutPage} />
+
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
+  )
+}
+
+export default App
